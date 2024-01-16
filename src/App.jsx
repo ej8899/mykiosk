d8e87508fdcd84573e5547a2b8b29985051ea77d
ej <ej8899@gmail.com>
2024-01-16 17:19:36 -0600

    update: clear css

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
-      <div className='bg-slate-500'>test</div>
-      <button className="bg-sky-500/100">test</button>
+      <NavBar/>
+      
     </>
   )
 }
