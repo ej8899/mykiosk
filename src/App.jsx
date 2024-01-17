b662e15aaf36557c2a99df63fee16c849568f263
ej <ej8899@gmail.com>
2024-01-17 04:35:58 -0700

    add: page rotation system

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './Navbar'
+import React, { useState, useEffect } from 'react';
+import Page1 from './pages/ClockOnly.jsx';
+// import Page2 from './Page2'; 
 
-function App() {
-  const [count, setCount] = useState(0)
+
+import './App.css'
+import NavBar from './components/Navbar'
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 1;
+  const minutesToChangePage = 5;
+
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentPage((prevPage) => (prevPage % totalPages) + 1);
+    }, minutesToChangePage * 60 * 1000); // Change every 5 minutes
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // Render different pages
+  const renderPage = () => {
+    switch (currentPage) {
+      case 1:
+        return <Page1 />;
+      // case 2:
+      //   return <Page2 />;
+      // Add more cases as needed
+      default:
+        return null;
+    }
+  };
 
   return (
-    <>
-      <NavBar/>
-      
-    </>
-  )
-}
+    <div className="kiosk-app">
+      {renderPage()}
+    </div>
+  );
+};
 
 export default App
