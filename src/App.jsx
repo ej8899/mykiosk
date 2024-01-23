8bf5f975b000cd6afc5ff2defdefb55bfbc80496
ej8899 <ej8899@gmail.com>
2024-01-23 17:35:14 -0500

    update: fix page timing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
 import Page1 from './pages/Dashboard.jsx';
 import Page2 from './pages/Countdown.jsx';
 import Page3 from './pages/Weather';
@@ -8,44 +9,47 @@ import Page5 from './pages/Photo';
 import './config.js';
 
 import './App.css'
-import NavBar from './components/Navbar'
+
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const totalPages = 5; // Update the total number of pages (numbering starts at 1)
   const primaryPage = 1;
+  const secondaryPages = [2, 3, 4, 5];
   const secondaryPageDuration = 30 * 1000; // 30 seconds
   const primaryPageDuration = 1 * 60 * 1000; // 1 minutes
 
+  const getRandomSecondaryPage = () => {
+    return secondaryPages[Math.floor(Math.random() * secondaryPages.length)];
+  };
+  
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentPage((prevPage) => {
         if (prevPage === primaryPage) {
-          // If the current page is the primary page, stay on it for the primary page duration
-          return primaryPage;
+          // If the current page is the primary page, switch to a random secondary page
+          return getRandomSecondaryPage();
         } else {
           // If the current page is a secondary page, switch back to the primary page
           return primaryPage;
         }
       });
-    }, secondaryPageDuration); // Change every 60 seconds
+    }, secondaryPageDuration); // Switch back to the primary page after secondaryPageDuration
 
     // Clear the interval when the component is unmounted
     return () => clearInterval(intervalId);
   }, []); // Empty dependency array to run only once on mount
 
   useEffect(() => {
-    const primaryPageIntervalId = setInterval(() => {
-      setCurrentPage((prevPage) => {
-        // Switch to the next secondary page after the primary page duration
-        return (prevPage % totalPages) + 1;
-      });
+    const primaryIntervalId = setInterval(() => {
+      setCurrentPage(getRandomSecondaryPage()); // Switch to a random secondary page after primaryPageDuration
     }, primaryPageDuration);
 
-    return () => clearInterval(primaryPageIntervalId);
-  }, []); 
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(primaryIntervalId);
+  }, []); // Empty dependency array to run only once on mount
 
-  // Render different pages
+  
   const renderPage = () => {
     switch (currentPage) {
       case 1:
