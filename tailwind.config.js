6792a90b84c10cbdae528c1d4287b4009cca988c
ej8899 <ej8899@gmail.com>
2024-01-23 20:12:10 -0500

    add: flowbite react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/**/*.{html,js}",'node_modules/flowbite-react/lib/esm/**/*.js',],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
+    import('flowbite/plugin'),
   ],
 }
