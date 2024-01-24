a8889b8a6697f40e3b10e42271b59397e61b0744
ej8899 <ej8899@gmail.com>
2024-01-23 22:44:21 -0500

    add: bg image rotator

---
+++ src/components/ImageRotator.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+const ImageRotator = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const imageList = [
+    'http://www.tcmd.com/lhl/final1.jpeg', 
+    'http://www.tcmd.com/lhl/final2.jpg', 
+    'http://www.tcmd.com/lhl/final3.jpeg',
+    'http://www.tcmd.com/lhl/final4.jpg', 
+    'http://www.tcmd.com/lhl/final5.jpg',
+    'http://www.tcmd.com/lhl/final6.jpg', 
+    'http://www.tcmd.com/lhl/final7.jpg',   
+  ];
+
+  useEffect(() => {
+    const initialIndex = Math.floor(Math.random() * imageList.length);
+    setCurrentImageIndex(initialIndex);
+    
+    const rotateInterval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageList.length);
+    }, 600000); // 10 minutes in milliseconds
+
+    return () => clearInterval(rotateInterval);
+  }, [currentImageIndex, imageList.length]);
+
+  const backgroundImageStyle = {
+    backgroundImage: `url(${imageList[currentImageIndex]})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh', 
+    width: '100%',
+    zIndex: -1,
+    position: 'absolute',
+  };
+
+  return <div style={backgroundImageStyle}></div>;
+};
+
+export default ImageRotator;
