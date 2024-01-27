08e859a10a4a352be97a4e1c1501037dee6c503b
ej8899 <ej8899@gmail.com>
2024-01-27 11:57:57 -0500

    update: streamline config data

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 
 export const globalconfig = {
   appVersion: "1.0",
-  appDeveloper: "Ernie Johnson",
+  appDeveloper: "ErnieJohnson.ca",
   appName: "CompanyKiosk",
 
   // first item is 'sticky' on the dashboard.  Basic HTML is allowed (it is DIV wrapped)
@@ -32,7 +32,7 @@ export const globalconfig = {
       details: 'this is event 4 but expired'
     },
     { date: '2024-01-27T12:00:00',
-      title: 'event is tomorrow',
+      title: 'event is 01.27 12:00',
       details: 'this is event 5 but tomorrow'
     },
     { date: '2024-01-26T21:20:00',
@@ -45,11 +45,6 @@ export const globalconfig = {
     },
   ],
 
-  // debug mode true or false
-  // usage is:  if (global.config.debug) console.log("debugging info here"); // or of course, other options for debug purposes
-  debug: true,
-
-
   // additional global vars and functions:
   timeClock: 12,       // 12 or 24 for clock setting - // todo - save to localstorage
   currentTheme: 'light', // light or dark
@@ -61,15 +56,7 @@ export const globalconfig = {
     youtube: "https://www.youtube.com/@oldmancodes",
   },
 
-  // usage: isFalsey(value) - is true if false value - checks Nan, 0, null, undefined, false, and ""
-  isFalsey: function (value) {
-    return !value;
-  },
 
-  // usage: global.config.goSleep(xxx).then(()=> { ... });
-  goSleep: function (ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  },
 };
 
 
